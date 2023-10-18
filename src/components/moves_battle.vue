ac05eff652f9d9e448a519858bcc0eddf3f7314a
schirmerp <schirmer.pc@gmail.com>
2023-10-18 19:28:41 -0400

    added 2 new characters to moves_battle

--- src/components/moves_battle.vue
+++ src/components/moves_battle.vue
@@ -31,12 +31,26 @@ const player3Moves = [
   { name: 'Minor Heal', cooldown: 3, currentCooldown: 0, action: function() { this.health += 10; return 0; } },
   { name: 'Charge', cooldown: 4, currentCooldown: 0, action: function(target) { return this.attack * 2.2; } },
 ];
+const player4Moves = [
+  { name: 'Butt Hug', cooldown: 0, currentCooldown: 0, action: function(target) { return this.attack * 2.5; } },
+  { name: 'Butt Pucker', cooldown: 2, currentCooldown: 0, action: function() { this.defense += 5; return 0; } },
+  { name: 'Butt Replenish', cooldown: 3, currentCooldown: 0, action: function() { this.health += 30; return 0; } },
+  { name: 'Shit on em', cooldown: 4, currentCooldown: 0, action: function(target) { return this.attack * 7.2; } },
+];
+const player5Moves = [
+  { name: 'Face Twist', cooldown: 0, currentCooldown: 0, action: function(target) { return this.attack * 2.5; } },
+  { name: 'Low Blow', cooldown: 2, currentCooldown: 0, action: function(target) { return this.attack * 3;} },
+  { name: 'Health Punch', cooldown: 3, currentCooldown: 0, action: function() { this.health += 25; return 0; } },
+  { name: 'Death Shove', cooldown: 5, currentCooldown: 0, action: function(target) { return this.attack * 5.2; } },
+];
 
 const player1 = new Player("Player 1", 100, 8, 5, './src/assets/p1.png', player1Moves);
 const player2 = new Player("Player 2", 100, 7, 6, './src/assets/p3.png', player2Moves);
 const player3 = new Player("Player 3", 100, 9, 4, './src/assets/pk.png', player3Moves);
+const player4 = new Player("Trump Butt", 100, 8, 6, './src/assets/trump.png', player4Moves);
+const player5 = new Player("Bob", 100, 10, 2, './src/assets/scary.png', player5Moves);
 
-const allPlayers = ref([player1, player2, player3]);
+const allPlayers = ref([player1, player2, player3, player4, player5]);
 const selectedPlayers = ref([]);
 const players = ref([]);
 const currentPlayerIndex = ref(null);
